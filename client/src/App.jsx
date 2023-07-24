d5d39557fc2e62fb5e9dbcc997122e74aac4c914
Felipe Suarez <felipesuarez1525@gmail.com>
2023-07-24 18:25:00 -0300

    fix: install  react-icons

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Components/Home/Home";
+import Navbar from "./Components/Navbar/Navbar";
+import Register from "./Components/Register/Register";
+import { Routes, Route, useLocation } from "react-router-dom";
+
+function App() {
+  const { pathname } = useLocation();
+  console.log(pathname);
+  return (
+    <div className="w-screen h-full flex flex-col justify-start items-center customBackground">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/registro" element={<Register />}></Route>
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.svg";
+import { FaBars, FaTimes } from "react-icons/fa";
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+  return (
+    <div
+      className={`${
+        nav ? "h-full" : "h-[70px]"
+      } w-full  lg:h-[70px] flex items-center justify-between bg-[#D6BED6] mt-14 px-10`}
+    >
+      <div className="flex box-border  items-center justify-start object-fit">
+        <img src={logo} alt="logo" className="block w-[121px] h-[114px]" />
+      </div>
+      <h1 className="flex-grow text-center text-gray-800 opacity-75 text-shadow ">Somos Jauría</h1>
+      <div className="hidden lg:flex  gap-x-6 justify-end">
+        <Link to="/redes" className="navButtons">
+          Redes
+        </Link>
+        <Link to="/login" className="navButtons">
+          Login
+        </Link>
+      </div>
+      {/* Hamburguesa */}
+      <div onClick={handleClick} className="lg:hidden z-20 p-4">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      {/* Mobile menu */}
+      <div
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-full bg-[#D6BED6] flex items-center z-10 justify-center "
+        }
+      >
+        <ul className="h-full flex flex-col gap-y-10 justify-center items-center ">
+          <li className="py-6 text-4xl primaryButton">
+            <Link onClick={handleClick} to="/login">
+              Login
+            </Link>
+          </li>
+          <li className="py-6 text-4xl primaryButton">
+            <Link onClick={handleClick} to="/redes">
+              Redes
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
